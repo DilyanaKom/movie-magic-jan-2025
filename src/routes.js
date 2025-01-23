17d04176a8c8a352742ff85f0efb46046919f070
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-01-23 15:24:17 +0200

    add routes

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import homeController from './controllers/home-controller.js';
+import { Router } from 'express';
+
+const routes = Router();
+routes.use(homeController);
+
+routes.get('*', (req, res) => {
+    res.render('404');
+})
+
+export default routes;
