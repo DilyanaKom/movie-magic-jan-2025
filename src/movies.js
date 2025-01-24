f7c20e23142f67c059f5bfe27bc388c1eec1023e
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-01-24 11:23:18 +0200

    Add dynamic movie rendering on home page

---
+++ src/movies.js
@@ -0,0 +1,32 @@
+export default [
+    {
+      title: "The Shadow Within",
+      genre: "Mystery",
+      description: "A brilliant detective unravels secrets in a small town haunted by its past, uncovering layers of intrigue.",
+      imageUrl: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1697879459i/200120223.jpg",
+    },
+    {
+      title: "Galactic Odyssey",
+      genre: "Sci-Fi",
+      description: "An interstellar crew fights to save their galaxy from destruction in a visually stunning space adventure.",
+      imageUrl: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1619660510i/1549283.jpg",
+    },
+    {
+      title: "Harmony Falls",
+      genre: "Romance",
+      description: "Two strangers find love and healing in a picturesque mountain town after facing personal tragedies.",
+      imageUrl: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781440593949/harmony-falls-9781440593949_lg.jpg",
+    },
+    {
+      title: "Beyond the Abyss",
+      genre: "Thriller",
+      description: "A marine biologist discovers a deep-sea conspiracy that could change the course of humanity forever.",
+      imageUrl: "https://m.media-amazon.com/images/I/41tCBR4ZAeL._SY445_SX342_.jpg",
+    },
+    {
+      title: "Rise of the Legends",
+      genre: "Action",
+      description: "A retired martial artist must return to the ring to protect his family from a powerful underground crime syndicate.",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/b/b2/Rise_of_the_Legend_poster.jpg",
+    },
+  ];
