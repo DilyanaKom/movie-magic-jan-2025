13a277eec00af26d1975a4c474e3ddb6faec2f75
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-02-04 15:44:06 +0200

    hash password on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,16 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     email: String,
     password: String,
 });
+
+//edgecase if there is a functionality to update email, then the saved hash would be hashed
+userSchema.pre('save', async function(){
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
 
 const User = model('User', userSchema);
 export default User;
