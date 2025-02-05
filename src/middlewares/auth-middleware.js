1eda180b1fae5e919569ca31f119cc8681a6be85
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-02-05 13:28:11 +0200

    Add dynamic navigation functionality

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -16,6 +16,7 @@ export const authMiddleware = (req, res, next) => {
         const decodedToken = jwt.verify(token, SECRET)
          //Attach decoded token to request
         req.user = decodedToken;
+        res.locals.user = decodedToken;
 
          next();
     }catch(err){
