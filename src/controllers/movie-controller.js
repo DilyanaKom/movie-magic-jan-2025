9fe9cafb5a922bb0db1b2c2f517a1d7625770230
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-02-05 13:01:38 +0200

    Show details buttons for creators

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -26,7 +26,8 @@ movieController.post('/create', async (req, res) => {
 movieController.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).populate('casts.cast').lean();
-    res.render('movie/details', {movie});
+    const isCreator = movie.creator && movie.creator?.toString() === req.user.id;
+    res.render('movie/details', {movie, isCreator});
 });
 
 movieController.get('/:movieId/attach-cast', async (req, res) => {
